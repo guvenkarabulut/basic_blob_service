d2452c97920aef67074cc538036636cac8ae424c
guvenkarabulut <guven.07.568@gmail.com>
2023-11-26 02:09:03 +0300

    added diffrent variants for items

--- basic_blob_service/app/controllers/items_controller.rb
+++ basic_blob_service/app/controllers/items_controller.rb
@@ -1,26 +1,62 @@
 class ItemsController < ApplicationController
-  skip_before_action :verify_authenticity_token, only: [:create, :update, :destroy]
-  before_action :set_item, only: %i[show]
+  include ItemsHelper
+
+  skip_before_action :verify_authenticity_token, only: %i[create destroy]
+  before_action :set_item, only: %i[show destroy]
+  after_action :clean_empty_folders_action, only: :destroy
+
   def create
     @item = Item.create(item_params)
-    @item.save
-    render json: @item
+    if @item.save
+      render json: @item
+    else
+      render json: { error: 'Failed to create item' }, status: :unprocessable_entity
+    end
   end
 
   def show
-    if @item.nil?
-      render json: {error: "İtem not found!"}, status: :bad_request
-      return
+    render_not_found unless @item
+
+    image_variant = case params[:v]
+                    when 'thumb' then :thumb
+                    when 'cover' then :cover
+                    else
+                      :default_variant
+                    end
+
+    @image = rails_blob_url(@item.item_image.variant(image_variant))
+    render json: { image: @image, item: @item }
+  end
+
+  def destroy
+    if @item
+      @photo = @item.item_image
+      @photo.purge_later
+      @item.destroy
+      clean_empty_folders(Rails.root.join('storage'))
+      render json: { message: 'Destroy is successful' }, status: :ok
+    else
+      render_not_found
     end
-    image = rails_blob_url(@item.item_image.variant(resize_to_limit: [500, 500]))
-    render json: {"image": image}
+  end
+
+  private
+
+  def clean_empty_folders_action
+    clean_empty_folders(Rails.root.join('storage'))
+    clean_empty_folders(Rails.root.join('storage'))
+
   end
 
   def set_item
-    @item = Item.find(params[:id])
+    @item = Item.find_by(id: params[:id])
   end
 
   def item_params
     params.permit(:item_image)
+  end
+
+  def render_not_found
+    render json: { error: 'Item not found!' }, status: :bad_request
   end
 end
--- basic_blob_service/app/models/item.rb
+++ basic_blob_service/app/models/item.rb
@@ -1,3 +1,7 @@
 class Item < ApplicationRecord
-  has_one_attached :item_image
+  has_one_attached :item_image do |attachable|
+    attachable.variant :default_variant, rotate: [0]
+    attachable.variant :thumb, resize_to_limit: [100, 100]
+    attachable.variant :cover, resize_to_fill: [500, 100]
+  end
 end
